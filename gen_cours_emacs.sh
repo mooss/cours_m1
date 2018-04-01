339e1e98b6f3036c8cc7a46677973feb5b00884d
mooss <mooss@protonmail.com>
2018-04-01 18:16:29 +0200

    Création du script de compilation des fichiers .org

---
+++ gen_cours_emacs.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+#############
+# arguments #
+#############
+verbose=$1
+
+#############
+# variables #
+#############
+orgfile='compilation_propre.org'
+texfile='compilation_propre.tex'
+latexcompilecmd="pdflatex -shell-escape -interaction nonstopmode -output-directory . $texfile"
+
+#########
+# setup #
+#########
+emacs $orgfile --batch -l ./org-mode_init.el org-mode_latex.el -f org-latex-export-to-latex --kill
+
+###############
+# compilation #
+###############
+echo -n "."
+if  [ "$verbose" != "verbose" ]
+then
+    $latexcompilecmd >/dev/null
+else
+    $latexcompilecmd
+fi
+
+echo -n "."
+$latexcompilecmd > /dev/null
+
+echo -n "."
+$latexcompilecmd > /dev/null
+
+###########
+# cleanup #
+###########
+rm -rf *.log *.out *.toc *.aux _minted-* *.tex
